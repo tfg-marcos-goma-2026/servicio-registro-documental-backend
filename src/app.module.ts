ece10cb09b340ec15f0a30184c19f1536e7aef05
Marcos1236 <marcosgomamartinez@gmail.com>
2026-05-19 11:32:14 +0200

    Instalar dependencias de infraestructura y resolver tsconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { DocumentsModule } from './documents/documents.module';
+import { BlockchainModule } from './blockchain/blockchain.module';
+import { VaultModule } from './vault/vault.module';
 
 @Module({
-  imports: [DocumentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DocumentsModule,
+    BlockchainModule,
+    VaultModule],
   controllers: [AppController],
   providers: [AppService],
 })
