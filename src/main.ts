05156388a655bc58f2f0d741ec1b24ae45251548
Marcos1236 <marcosgomamartinez@gmail.com>
2026-05-23 17:05:19 +0200

    Mover el prefijo de swagger

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api/v1/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
